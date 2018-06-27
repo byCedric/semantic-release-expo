d5907d689fd38b57f81d23f60d50f8b2fce3e61c
Cedric van Putten <me@bycedric.com>
2018-06-28 01:15:25 +0200

    chore: update semantic release config with the peakfijn conventions
    This makes sure we have readably changelogs.

---
+++ .releaserc.js
@@ -0,0 +1,39 @@
+const commitTypes = require('commit-types-peakfijn');
+
+module.exports = {
+	branch: 'master',
+	repositoryUrl: 'https://github.com/bycedric/semantic-release-expo.git',
+	tagFormat: '${version}',
+	analyzeCommits: [
+		{
+			path: '@semantic-release/commit-analyzer',
+			preset: 'peakfijn',
+			releaseRules: Object.keys(commitTypes)
+				.map(type => ({ type, release: commitTypes[type].release }))
+				.filter(rule => !!rule.release),
+		},
+	],
+	generateNotes: [
+		{
+			path: '@semantic-release/release-notes-generator',
+			preset: 'peakfijn',
+		},
+	],
+	verifyConditions: [
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		'semantic-release-git-branches',
+	],
+	prepare: [
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		{
+			path: 'semantic-release-git-branches',
+			message: 'chore: create new release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			branchMerges: [
+				'develop',
+				'master',
+			],
+		},
+	],
+};
