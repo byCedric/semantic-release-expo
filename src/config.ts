fc6f0937398a8de2b387abff95458171a6161f34
Cedric van Putten <me@bycedric.com>
2018-07-11 20:14:29 +0200

    feature: inherit configuration from prepare when verifying

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { Config } from './types';
+import { Config, Context } from './types';
 import { MANIFEST_FILE } from './expo';
 
 /**
@@ -11,4 +11,30 @@ export function getManifestFiles(config: Config): string[] {
 	}
 
 	return config.manifests;
+}
+
+/**
+ * Get the full prepare configuration of the prepare step for this extension.
+ * It's mostly used for the verify conditions step to inherit the configuration.
+ */
+export function getPrepareConfig(context: Context): Config | undefined {
+	if (context.options && context.options.prepare) {
+		const plugins = Array.isArray(context.options.prepare)
+			? context.options.prepare
+			: [context.options.prepare];
+
+		return plugins.find(config => config.path && config.path === 'semantic-release-expo');
+	}
+}
+
+/**
+ * Inherit all configuration from the possible prepare step.
+ * This makes sure we don't have to copy all settings when verifying.
+ */
+export function inheritPrepareConfig(config: Config, context: Context): Config {
+	const prepareConfig = getPrepareConfig(context) || {};
+
+	return {
+		manifests: config.manifests || prepareConfig.manifests
+	};
 }
--- test/scripts/verify-conditions.test.ts
+++ test/scripts/verify-conditions.test.ts
@@ -7,6 +7,19 @@ import verifyConditions from '../../src/scripts/verify-conditions';
 const SemanticReleaseError = require('@semantic-release/error');
 
 describe('scripts/verify-conditions', () => {
+	const createContextWithPrepare = (prepare?: any) => ({
+		options: {
+			prepare,
+			branch: 'master',
+			repositoryUrl: 'https://github.com/bycedric/semantic-release-expo',
+			tagFormat: '${version}',
+		},
+		logger: {
+			log: jest.fn(),
+			error: jest.fn(),
+		},
+	});
+
 	it('reads manifest and logs name', async () => {
 		const config = {
 			manifests: [
@@ -14,12 +27,7 @@ describe('scripts/verify-conditions', () => {
 				'app.staging.json',
 			],
 		};
-		const context = {
-			logger: {
-				log: jest.fn(),
-				error: jest.fn(),
-			},
-		};
+		const context = createContextWithPrepare();
 
 		const firstMeta = {
 			filename: 'app.json',
@@ -57,15 +65,53 @@ describe('scripts/verify-conditions', () => {
 
 	it('throws when read manifest failed', async () => {
 		const config = {};
-		const context = {
-			logger: {
-				log: jest.fn(),
-				error: jest.fn(),
-			},
-		};
+		const context = createContextWithPrepare();
 
 		readManifests.mockRejectedValue(new Error());
 
 		expect(verifyConditions(config, context)).rejects.toThrowError();
+	});
+
+	it('inherits prepare configration without verify conditions configuration', async () => {
+		const config = {};
+		const manifests = ['app.production.json', 'app.staging.json'];
+		const context = createContextWithPrepare([
+			{ path: '@semantic-release/changelog' },
+			{ path: '@semantic-release/npm' },
+			{ path: 'semantic-release-expo', manifests },
+		]);
+
+		const firstMeta = {
+			filename: 'app.production.json',
+			content: '{ "name": "test" }',
+			manifest: { name: 'test' },
+		};
+
+		const secondMeta = {
+			filename: 'app.staging.json',
+			content: '{ "name": "test-staging" }',
+			manifest: { name: 'test-staging' },
+		};
+
+		readManifests.mockResolvedValue([firstMeta, secondMeta]);
+
+		await verifyConditions(config, context);
+
+		expect(readManifests).toBeCalled();
+		expect(context.logger.log).toHaveBeenNthCalledWith(
+			1,
+			'Found %s manifest for %s in %s',
+			'Expo',
+			'test',
+			'app.production.json',
+		);
+
+		expect(context.logger.log).toHaveBeenNthCalledWith(
+			2,
+			'Found %s manifest for %s in %s',
+			'Expo',
+			'test-staging',
+			'app.staging.json',
+		);
 	});
 });
