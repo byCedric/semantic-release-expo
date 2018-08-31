0257293ba420128cf16c975fb174c5e4b301260f
Cedric van Putten <byCedric@users.noreply.github.com>
2018-08-31 11:39:48 +0200

    style: use tabs for indentation in android version bumper

--- src/version-bumpers/platform-android.ts
+++ src/version-bumpers/platform-android.ts
@@ -6,7 +6,7 @@ const bumpPlatformAndroid: VersionBumper = (meta, context) => {
 	const sdkVersion = semver.coerce(meta.manifest.sdkVersion || 0);
 	const android = getAndroidPlatform(meta.manifest);
 	const oldVersion = android.versionCode || 0;
-    const nextRelease = semver.coerce(context.nextRelease!.version);
+	const nextRelease = semver.coerce(context.nextRelease!.version);
 	const newVersion = sdkVersion!.major * 10000000 + nextRelease!.major * 10000 + nextRelease!.minor * 100 + nextRelease!.patch;
 
 	context.logger.log(
