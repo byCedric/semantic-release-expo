9a5048e463cdb8eec3de8b22303d3e977445eed3
Nick Bordeau <nickbordeau@gmail.com>
2021-08-28 10:56:04 -0400

    feat: allow config.js files
    - updated to allow `app.config.js` files

--- src/expo.ts
+++ src/expo.ts
@@ -1,7 +1,7 @@
-import detectIndent from 'detect-indent';
-import detectNewline from 'detect-newline';
-import { readFile, writeJson } from 'fs-extra';
-import { Context } from './types';
+import detectIndent from "detect-indent";
+import detectNewline from "detect-newline";
+import { readFile, writeJson } from "fs-extra";
+import { Context } from "./types";
 
 /**
  * A manifest-light definition to use for typehinting.
@@ -30,24 +30,28 @@ export interface ManifestMeta {
 /**
  * The name of the default Expo manifest file.
  */
-export const MANIFEST_FILE = 'app.json';
+export const MANIFEST_FILE = "app.json";
 
 /**
  * The default indentation to use when no indentation is found.
  */
-export const DEFAULT_INDENT = '  ';
+export const DEFAULT_INDENT = "  ";
 
 /**
  * The default newline character to use when no existing was detected.
  */
-export const DEFAULT_NEWLINE = '\n';
+export const DEFAULT_NEWLINE = "\n";
 
 /**
  * Log information about the manifest which is related to the error.
  */
 export function logManifestFromError(context: Context, error: any) {
 	if (error && error.expo) {
-		context.logger.log('Error encountered for %s manifest %s', 'Expo', error.expo);
+		context.logger.log(
+			"Error encountered for %s manifest %s",
+			"Expo",
+			error.expo
+		);
 	}
 }
 
@@ -56,8 +60,12 @@ export function logManifestFromError(context: Context, error: any) {
  */
 export async function readManifest(filename: string): Promise<ManifestMeta> {
 	try {
-		const content = await readFile(filename, 'utf8');
-		const manifest = JSON.parse(content).expo;
+		const isNewAppConfig = filename.includes("config.js");
+
+		const content = isNewAppConfig
+			? require(filename)
+			: await readFile(filename, "utf8");
+		const manifest = isNewAppConfig ? content : JSON.parse(content).expo;
 
 		return { filename, content, manifest };
 	} catch (error) {
@@ -69,7 +77,9 @@ export async function readManifest(filename: string): Promise<ManifestMeta> {
 /**
  * Read a list of Expo mannifest files and return the parsed JSON.
  */
-export async function readManifests(filenames: string[]): Promise<ManifestMeta[]> {
+export async function readManifests(
+	filenames: string[]
+): Promise<ManifestMeta[]> {
 	return await Promise.all(filenames.map(readManifest));
 }
 
@@ -80,7 +90,11 @@ export async function writeManifest(meta: ManifestMeta, manifest: Manifest) {
 	const { indent } = detectIndent(meta.content) || { indent: DEFAULT_INDENT };
 	const newline = detectNewline(meta.content) || DEFAULT_NEWLINE;
 
-	await writeJson(meta.filename, { expo: manifest }, { spaces: indent, EOL: newline });
+	await writeJson(
+		meta.filename,
+		{ expo: manifest },
+		{ spaces: indent, EOL: newline }
+	);
 }
 
 /**
@@ -88,7 +102,7 @@ export async function writeManifest(meta: ManifestMeta, manifest: Manifest) {
  * This will fallback to the default from Expo itself.
  */
 export function getPlatforms(manifest: Manifest) {
-	return manifest.platforms || ['android', 'ios'];
+	return manifest.platforms || ["android", "ios"];
 }
 
 /**
@@ -102,5 +116,5 @@ export function getAndroidPlatform(manifest: Manifest) {
  * Get the platform settings for iOS, if available.
  */
 export function getIosPlatform(manifest: Manifest) {
-	return manifest.ios || { buildNumber: '' };
+	return manifest.ios || { buildNumber: "" };
 }
